70856978f05b7babf3e9030d9e12fcf3d7ddee15
GaelRI <91554106+GaelRI@users.noreply.github.com>
2021-11-24 22:41:21 -0600

    Update FT7.m

--- FT7.m
+++ FT7.m
@@ -1,17 +1,17 @@
 % Octave Script
-% Title         :F. trascendentes: trigonométricas, logarítmicas y exponenciales.
+% Title         :F. trascendentes: trigonomÃ©tricas, logarÃ­tmicas y exponenciales.
 % Description   :Script para funciones algebraicas
-% Author        :Gael Franco García
+% Author        :Gael Franco GarcÃ­a
 % Date          :202123508
 % Version       :1
 % Usage         :octave> /path/F.T.7
-% Notes         :Requiere aplicación octave y usar linea de comandos
+% Notes         :Requiere aplicaciÃ³n octave y usar linea de comandos
 
 clear
 syms x
-% función
+% funciÃ³n
 fx= sin(x);
-% plotear función
+% plotear funciÃ³n
 ezplot(fx)
 % plano cartesiano
 hold on;
@@ -20,3 +20,5 @@ plot([-20 20],[0 0],'m-',"linewidth",2,"markersize",8);
 plot([-20 20],[0 0],'m-',"linewidth",2,"markersize",8);
 title(['fx= sin(x) Funcion Suprayectiva']);
 xlabel("Contradominio le corresponde el mismo rango dado")
+disp('La funcion c(v)=sin(v) es Sobrayectiva');
+disp(['Sus elementos pasan por varios puntos de la imagen incluyendo negativos ']);
