3fd81beb23e362be4172eed8ca1f4db603b4b4de
GaelRI <91554106+GaelRI@users.noreply.github.com>
2021-11-24 22:42:52 -0600

    Update FT8.m

--- FT8.m
+++ FT8.m
@@ -1,22 +1,24 @@
 % Octave Script
-% Title         :F. trascendentes: trigonométricas, logarítmicas y exponenciales.
+% Title         :F. trascendentes: trigonomÃ©tricas, logarÃ­tmicas y exponenciales.
 % Description   :Script para funciones algebraicas
 % Author        :Gael Franco 
 % Date          :202123508
 % Version       :1
 % Usage         :octave> /path/F.T.8
-% Notes         :Requiere aplicación octave y usar linea de comandos
+% Notes         :Requiere aplicaciÃ³n octave y usar linea de comandos
 
 clear
 syms x
-% función
+% funciÃ³n
 fx= cos(x);
-% plotear función
+% plotear funciÃ³n
 ezplot(fx)
 % plano cartesiano
 hold on;
 grid on; 
 plot([-20 20],[0 0],'m-',"linewidth",2,"markersize",8);
 plot([-20 20],[0 0],'m-',"linewidth",2,"markersize",8);
-title(['fx= cos(x) Funcion Suprayectiva']);
+title(['fx= cos(x) Funcion Sobrayectiva']);
 xlabel("Contradominio le corresponde el mismo rango dado")
+disp('La funcion f(x)=cos(x) es Sobrayectiva');
+disp(['Sus elementos pasan por varios puntos de la imagen incluyendo negativos ']);
