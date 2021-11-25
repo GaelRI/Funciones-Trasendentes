a2332e1ed55f67868d20d23e93394a97eaf40f39
GaelRI <91554106+GaelRI@users.noreply.github.com>
2021-11-24 22:48:21 -0600

    Update FT14.m

--- FT14.m
+++ FT14.m
@@ -1,22 +1,23 @@
 % Octave Script
-% Title         :F. trascendentes: trigonométricas, logarítmicas y exponenciales.
+% Title         :F. trascendentes: trigonomÃ©tricas, logarÃ­tmicas y exponenciales.
 % Description   :Script para funciones algebraicas
-% Author        : Gael Franco García
+% Author        : Gael Franco GarcÃ­a
 % Date          :202123508
 % Version       :1
 % Usage         :octave> /path/F.T.14
-% Notes         :Requiere aplicación octave y usar linea de comandos
+% Notes         :Requiere aplicaciÃ³n octave y usar linea de comandos
 
 clear
 syms x
-% función
+% funciÃ³n
 fx= 9^x-2;
-% plotear función
+% plotear funciÃ³n
 ezplot(fx);
 % plano cartesiano
 hold on;
 grid on; 
 plot([-20 20],[0 0],'m-',"linewidth",2,"markersize",8);
 plot([-20 20],[0 0],'m-',"linewidth",2,"markersize",8);
-title(['fx= 9^x-2 Funcion Suprayectiva']);
+title(['fx= 9^x-2 Funcion Inyectiva']);
 xlabel("Contradominio le corresponde el mismo rango dado")
+disp('Pasa por la imagen tomando solo un elemento');
