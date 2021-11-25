4d6e9134fa5a96e1fd8f0e11057c8baf7990a5c6
GaelRI <91554106+GaelRI@users.noreply.github.com>
2021-11-24 22:35:32 -0600

    Update FT2.m

--- FT2.m
+++ FT2.m
@@ -1,22 +1,24 @@
 % Octave Script
-% Title         :F. trascendentes: trigonométricas, logarítmicas y exponenciales.
+% Title         :F. trascendentes: trigonomÃ©tricas, logarÃ­tmicas y exponenciales.
 % Description   :Script para funciones algebraicas
-% Author        :Gael Franco García
+% Author        :Gael Franco GarcÃ­a
 % Date          :202123508
 % Version       :1
 % Usage         :octave> /path/F.T.2
-% Notes         :Requiere aplicación octave y usar linea de comandos
+% Notes         :Requiere aplicaciÃ³n octave y usar linea de comandos
 
 clear
 syms x
-% función
+% funciÃ³n
 fx= (3^x);
-% plotear función
+% plotear funciÃ³n
 ezplot(fx)
 % plano cartesiano
 hold on;
 grid on; 
 plot([-20 20],[0 0],'m-',"linewidth",2,"markersize",8);
 plot([-20 20],[0 0],'m-',"linewidth",2,"markersize",8);
-title(['fx=(3^x) Funcion Suprayectiva']);
+title(['fx=(3^x) Funcion Inyectiva']);
 xlabel("Contradominio le corresponde el mismo rango dado")
+disp(['La Funcion f(x)=3.^x es Inyectiva']);
+disp('Muestra un solo punto de la imagen con uno del dominio');
