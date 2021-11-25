209a810f08a9327d76a9dfc02b1d0a76c203460f
GaelRI <91554106+GaelRI@users.noreply.github.com>
2021-11-24 22:48:56 -0600

    Update FT15.m

--- FT15.m
+++ FT15.m
@@ -1,22 +1,24 @@
 % Octave Script
-% Title         :F. trascendentes: trigonométricas, logarítmicas y exponenciales.
+% Title         :F. trascendentes: trigonomÃ©tricas, logarÃ­tmicas y exponenciales.
 % Description   :Script para funciones algebraicas
-% Author        :Gael Franco García
+% Author        :Gael Franco GarcÃ­a
 % Date          :202123508
 % Version       :1
 % Usage         :octave> /path/F.T.15
-% Notes         :Requiere aplicación octave y usar linea de comandos
+% Notes         :Requiere aplicaciÃ³n octave y usar linea de comandos
 
 clear
 syms x
-% función
+% funciÃ³n
 fx= exp(1)^x^9*x+18;
-% plotear función
+% plotear funciÃ³n
 ezplot(fx);
 % plano cartesiano
 hold on;
 grid on; 
 plot([-20 20],[0 0],'m-',"linewidth",2,"markersize",8);
 plot([-20 20],[0 0],'m-',"linewidth",2,"markersize",8);
-title(['fx= exp(1)^x^9*x+18 Funcion Suprayectiva']);
+title(['fx= exp(1)^x^9*x+18 Funcion Inyectiva']);
 xlabel("Contradominio le corresponde el mismo rango dado")
+disp(['La funcion e^9x + 18 es Inyectiva ']);
+disp('Toma un solo elemento por imagen y va hacia arriba por que es exponencial');
