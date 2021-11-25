0e1d9da794f4dccf478601fb2d45900343c827f8
GaelRI <91554106+GaelRI@users.noreply.github.com>
2021-11-24 22:46:39 -0600

    Update FT12.m

--- FT12.m
+++ FT12.m
@@ -1,22 +1,24 @@
 % Octave Script
-% Title         :F. trascendentes: trigonométricas, logarítmicas y exponenciales.
+% Title         :F. trascendentes: trigonomÃ©tricas, logarÃ­tmicas y exponenciales.
 % Description   :Script para funciones algebraicas
-% Author        :Gael Franco García
+% Author        :Gael Franco GarcÃ­a
 % Date          :202123508
 % Version       :1
 % Usage         :octave> /path/F.T.12
-% Notes         :Requiere aplicación octave y usar linea de comandos
+% Notes         :Requiere aplicaciÃ³n octave y usar linea de comandos
 
 clear
 syms x
-% función
+% funciÃ³n
 fx= cot(x);
-% plotear función
+% plotear funciÃ³n
 ezplot(fx)
 % plano cartesiano
 hold on;
 grid on; 
 plot([-20 20],[2 2],'m-',"linewidth",2,"markersize",8);
 plot([-20 20],[-2 -2],'m-',"linewidth",2,"markersize",8);
-title(['fx= cot(x) Funcion Suprayectiva']);
+title(['fx= cot(x) Funcion]);
 xlabel("Contradominio le corresponde el mismo rango dado")
+disp('La funcion i=(3/7)^r es Sobrayectiva');
+disp(['Sus elementos pasan por varios puntos de la imagen incluyendo negativos ']);
